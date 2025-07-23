11e6424b220ca18292339b464acc752a1132a4de
kreideprinzV <160458856+kreideprinzV@users.noreply.github.com>
2025-07-23 09:41:53 +0800

    Delete P-repo directory

--- P-repo/inventory/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, InventoryItemViewSet
-
-router = DefaultRouter()
-router.register('categories', CategoryViewSet, basename='category')
-router.register('inventory-items', InventoryItemViewSet, basename='inventoryitem')
-
-urlpatterns = router.urls
--- P-repo/order/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-from .models import Order
-from .serializers import OrderSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-
-class OrderViewSet(viewsets.ModelViewSet):
-    queryset = Order.objects.all().order_by('-created_at')
-    serializer_class = OrderSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
